2d8dc0e0a4dda14f498612875ff429ade8308b93
Nanorneirbo <47759615+Nanorneirbo@users.noreply.github.com>
2019-03-20 20:04:01 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,393 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] initialBoard = new int[]{1, 0, 2, 4, 5, 3, 7, 8, 6};
+        int i;
+        int[] winningBoard = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 0};
+        int[] leftCheck = new int[9];
+        int[] rightCheck = new int[9];
+        int[] upCheck = new int[9];
+        int[] downCheck = new int[9];
+
+
+        Methods arrays = new Methods(initialBoard, winningBoard);
+        ArrayList<int[]> closedList = new ArrayList<int[]>();
+        ArrayList<int[]> openList = new ArrayList<int[]>();
+        ArrayList<Integer> heuristicList = new ArrayList<Integer>();
+        ArrayList<Integer> gList = new ArrayList<Integer>();
+        ArrayList<Integer> fandgList = new ArrayList<Integer>();
+		/*
+		Maintain a seperate list of g values
+		whatever g list I just used the next g states are G plus 1
+		f gets tested on the combined g and f list
+		*/
+
+
+
+        // while loop controller
+        int g = 0;
+        //create a board checker
+
+        int controller = arrays.heuristic(initialBoard);
+
+
+        while (controller !=0&& g<100 ) {
+            System.out.println();
+            System.out.println("Loop " + g + " size of open list: " + openList.size());
+            // find the highest index
+            int index = arrays.indexOfZero(initialBoard);
+
+            // Switch statements that will call methods to move depending on where the 0 is
+            switch (index) {
+                case 0:
+                    for (i = 0; i < 9; i++) {
+                        rightCheck[i] = initialBoard[i];
+                        downCheck[i] = initialBoard[i];
+                    }
+
+
+                    arrays.moveRight(rightCheck);
+                    arrays.moveDown(downCheck);
+
+                    if((!closedList.contains(rightCheck))&&(!openList.contains(rightCheck))) {
+                        openList.add(rightCheck);
+                        gList.add(g);
+                        heuristicList.add(arrays.heuristic(rightCheck));
+                    }
+
+                    if((!closedList.contains(downCheck))&&(!openList.contains(downCheck))) {
+                        openList.add(downCheck);
+                        gList.add(g);
+                        heuristicList.add(arrays.heuristic(downCheck));
+                    }
+
+                    System.out.println("Case 0");
+                    break;
+                case 1:
+                    for (i = 0; i < 9; i++) {
+                        rightCheck[i] = initialBoard[i];
+                        downCheck[i] = initialBoard[i];
+                        leftCheck[i] = initialBoard[i];
+                    }
+
+                    arrays.moveRight(rightCheck);
+                    arrays.moveDown(downCheck);
+                    arrays.moveLeft(leftCheck);
+
+                    if((!closedList.contains(rightCheck))&&(!openList.contains(rightCheck))) {
+                        openList.add(rightCheck);
+                        gList.add(g);
+                        heuristicList.add(arrays.heuristic(rightCheck));
+                    }
+
+                    if((!closedList.contains(downCheck))&&(!openList.contains(downCheck))) {
+                        openList.add(downCheck);
+                        gList.add(g);
+                        heuristicList.add(arrays.heuristic(downCheck));
+                    }
+
+                    if((!closedList.contains(leftCheck))&&(!openList.contains(leftCheck))) {
+                        openList.add(leftCheck);
+                        gList.add(g);
+                        heuristicList.add(arrays.heuristic(leftCheck));
+                    }
+
+                    System.out.println("Case 1");
+                    break;
+                case 2:
+                    for (i = 0; i < 9; i++) {
+                        leftCheck[i] = initialBoard[i];
+                        downCheck[i] = initialBoard[i];
+                    }
+
+                    arrays.moveLeft(leftCheck);
+                    arrays.moveDown(downCheck);
+
+                    if((!closedList.contains(leftCheck))&&(!openList.contains(leftCheck))) {
+                        openList.add(leftCheck);
+                        heuristicList.add(arrays.heuristic(leftCheck));
+                        gList.add(g);
+                    }
+
+                    if((!closedList.contains(downCheck))&&(!openList.contains(downCheck))) {
+                        openList.add(downCheck);
+                        gList.add(g);
+                        heuristicList.add(arrays.heuristic(downCheck));
+                    }
+                    System.out.println("First entry in open list:");
+                    for(i=0;i<9; i++){
+                        System.out.print(openList.get(0)[i] + ",");
+                    }
+
+                    System.out.println();
+                    System.out.println("Second entry in open list:");
+                    for(i=0;i<9; i++){
+                        System.out.print(openList.get(1)[i] + ",");
+                    }
+
+                    System.out.println("Case 2");
+                    break;
+                case 3:
+                    for (i = 0; i < 9; i++) {
+                        rightCheck[i] = initialBoard[i];
+                        downCheck[i] = initialBoard[i];
+                        upCheck[i] = initialBoard[i];
+                    }
+
+                    arrays.moveRight(rightCheck);
+                    arrays.moveDown(downCheck);
+                    arrays.moveUp(upCheck);
+
+                    if((!closedList.contains(downCheck))&&(!openList.contains(downCheck))) {
+                        openList.add(downCheck);
+                        heuristicList.add(arrays.heuristic(downCheck));
+                        gList.add(g);
+                    }
+
+                    if((!closedList.contains(rightCheck))&&(!openList.contains(rightCheck))) {
+                        openList.add(rightCheck);
+                        heuristicList.add(arrays.heuristic(rightCheck));
+                        gList.add(g);
+                    }
+                    if((!closedList.contains(upCheck))&&(!openList.contains(upCheck))) {
+                        openList.add(upCheck);
+                        heuristicList.add(arrays.heuristic(upCheck));
+                        gList.add(g);
+                    }
+
+
+                    System.out.println("Case 3");
+                    break;
+                case 4:
+                    for (i = 0; i < 9; i++) {
+                        rightCheck[i] = initialBoard[i];
+                        downCheck[i] = initialBoard[i];
+                        leftCheck[i] = initialBoard[i];
+                        upCheck[i] = initialBoard[i];
+                    }
+                    arrays.moveRight(rightCheck);
+                    arrays.moveDown(downCheck);
+                    arrays.moveLeft(leftCheck);
+                    arrays.moveUp(upCheck);
+
+                    if((!closedList.contains(downCheck))&&(!openList.contains(downCheck))) {
+                        openList.add(downCheck);
+                        heuristicList.add(arrays.heuristic(downCheck));
+                        gList.add(g);
+                    }
+                    if((!closedList.contains(upCheck))&&(!openList.contains(upCheck))) {
+                        openList.add(upCheck);
+                        heuristicList.add(arrays.heuristic(upCheck));
+                        gList.add(g);
+                    }
+                    if((!closedList.contains(rightCheck))&&(!openList.contains(rightCheck))) {
+                        openList.add(rightCheck);
+                        heuristicList.add(arrays.heuristic(rightCheck));
+                        gList.add(g);
+                    }
+                    if((!closedList.contains(leftCheck))&&(!openList.contains(leftCheck))) {
+                        openList.add(leftCheck);
+                        heuristicList.add(arrays.heuristic(leftCheck));
+                        gList.add(g);
+                    }
+
+                    break;
+                case 5:
+                    // down - up - Left
+                    for (i = 0; i < 9; i++) {
+                        leftCheck[i] = initialBoard[i];
+                        upCheck[i] = initialBoard[i];
+                        downCheck[i] = initialBoard[i];
+                    }
+                    arrays.moveLeft(leftCheck);
+                    arrays.moveUp(upCheck);
+                    arrays.moveDown(downCheck);
+                    System.out.println("Case 5");
+
+                    if((!closedList.contains(leftCheck))&&(!openList.contains(leftCheck))) {
+                        openList.add(leftCheck);
+                        heuristicList.add(arrays.heuristic(leftCheck));
+                        gList.add(g);
+                    }
+                    if((!closedList.contains(downCheck))&&(!openList.contains(downCheck))) {
+                        openList.add(downCheck);
+                        heuristicList.add(arrays.heuristic(downCheck));
+                        gList.add(g);
+                    }
+                    if((!closedList.contains(upCheck))&&(!openList.contains(upCheck))) {
+                        openList.add(upCheck);
+                        heuristicList.add(arrays.heuristic(upCheck));
+                        gList.add(g);
+                    }
+                    //see the open list
+                    System.out.println("First entry in open list:");
+                    for(i=0;i<9; i++){
+                        System.out.print(openList.get(0)[i] + ",");
+                    }
+
+                    System.out.println();
+                    System.out.println("Second entry in open list:");
+                    for(i=0;i<9; i++){
+                        System.out.print(openList.get(1)[i] + ",");
+                    }
+                    System.out.println();
+                    System.out.println("Third entry in open list:");
+                    for(i=0;i<9; i++){
+                        System.out.print(openList.get(2)[i] + ",");
+                    }
+
+
+                    break;
+                case 6:
+                    for (i = 0; i < 9; i++) {
+                        upCheck[i] = initialBoard[i];
+                        rightCheck[i] = initialBoard[i];
+                    }
+                    arrays.moveUp(upCheck);
+                    arrays.moveRight(rightCheck);
+
+                    if((!closedList.contains(upCheck))&&(!openList.contains(upCheck))) {
+                        openList.add(upCheck);
+                        heuristicList.add(arrays.heuristic(upCheck));
+                        gList.add(g);
+                    }
+                    if((!closedList.contains(rightCheck))&&(!openList.contains(rightCheck))) {
+                        openList.add(rightCheck);
+                        heuristicList.add(arrays.heuristic(rightCheck));
+                        gList.add(g);
+                    }
+
+
+                    System.out.println("Case 6");
+                    break;
+                case 7:
+                    //copy the boards
+                    for (i = 0; i < 9; i++) {
+                        upCheck[i] = initialBoard[i];
+                        rightCheck[i] = initialBoard[i];
+                        leftCheck[i] = initialBoard[i];
+                    }
+                    //move the 0's
+                    arrays.moveUp(upCheck);
+                    arrays.moveRight(rightCheck);
+                    arrays.moveLeft(leftCheck);
+
+                    if((!closedList.contains(upCheck))&&(!openList.contains(upCheck))) {
+                        openList.add(upCheck);
+                        heuristicList.add(arrays.heuristic(upCheck));
+                        gList.add(g);
+                    }
+                    if((!closedList.contains(rightCheck))&&(!openList.contains(rightCheck))) {
+                        openList.add(rightCheck);
+                        heuristicList.add(arrays.heuristic(rightCheck));
+                        gList.add(g);
+                    }
+                    if((!closedList.contains(leftCheck))&&(!openList.contains(leftCheck))) {
+                        openList.add(leftCheck);
+                        heuristicList.add(arrays.heuristic(leftCheck));
+                        gList.add(g);
+                    }
+                    System.out.println("Case 7");
+                    break;
+                case 8:
+                    for (i = 0; i < 9; i++) {
+                        upCheck[i] = initialBoard[i];
+                        leftCheck[i] = initialBoard[i];
+                    }
+
+                    arrays.moveUp(upCheck);
+                    arrays.moveLeft(leftCheck);
+
+                    if((!closedList.contains(leftCheck))&&(!openList.contains(leftCheck))) {
+                        openList.add(leftCheck);
+                        heuristicList.add(arrays.heuristic(leftCheck));
+                        gList.add(g);
+                    }
+                    if((!closedList.contains(upCheck))&&(!openList.contains(upCheck))) {
+                        openList.add(upCheck);
+                        heuristicList.add(arrays.heuristic(upCheck));
+                        gList.add(g);
+                    }
+
+                    System.out.println("Case 8");
+                    break;
+            }
+            g++;
+
+
+
+           /* //find index of lowest heuristic -- moved this down
+           int ind = arrays.indexNextState(fandgList);
+
+            //set up next board
+            for (i = 0; i < 9; i++) {
+                initialBoard[i] = (openList.get(ind)[i]);
+                //  System.out.print(initialBoard[i] + ",");
+            }
+*/
+            System.out.println();
+            System.out.print("Glist: ");
+            for (i = 0; i < gList.size(); i++) {
+
+                System.out.print(gList.get(i)+", ");
+            }
+            System.out.println();
+            System.out.print("Flist: ");
+            for (i = 0; i < gList.size(); i++) {
+
+                System.out.print(heuristicList.get(i)+", ");
+            }
+
+            //gList.add(arrays.gValue(gList, heuristicList));
+
+            System.out.println();
+            System.out.print("Combined heuristic : ");
+            for(i=0; i< heuristicList.size(); i++){
+                fandgList.add(gList.get(i)+heuristicList.get(i));
+                System.out.print(fandgList.get(i) + ",");
+            }
+            System.out.println();
+            System.out.println("Lowest Heurisitic : " +arrays.indexNextState(fandgList));
+
+            //find index of lowest heuristic
+            int ind = arrays.indexNextState(fandgList);
+
+            //set up next board
+            for (i = 0; i < 9; i++) {
+                initialBoard[i] = (openList.get(ind)[i]);
+                //  System.out.print(initialBoard[i] + ",");
+            }
+
+
+
+            //make this a remove function and add a closed list.
+            closedList.add(openList.get(arrays.indexNextState(fandgList)));
+            //openList.remove(arrays.indexNextState(fandgList));
+            //heuristicList.remove(arrays.indexNextState(fandgList));
+            //gList.remove(arrays.indexNextState(fandgList));
+
+            controller = arrays.heuristic(initialBoard);
+
+            System.out.println();
+            System.out.println("Next State to Check : ");
+            for(i=0; i<9; i++){
+                System.out.print(initialBoard[i] + ",");
+            }
+            fandgList.clear();
+        }
+
+        if(controller == 0){
+            System.out.println();
+            System.out.println("You found the correct state! After XXX Moves");
+        }
+
+
+    }
+
+}
---
+++ src/com/company/Methods.java
@@ -0,0 +1,154 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.*;
+
+public class Methods {
+    private int[] startingState = new int[9];
+    private int[] victoryState = new int[9];
+
+    public void setStartingState(int[] startingState) {
+        this.startingState = startingState;
+    }
+
+    public void setVictoryState(int[] startingState) {
+        this.victoryState = victoryState;
+    }
+
+    public int[] getStartingState() {
+        return startingState;
+    }
+
+    public int[] getVictoryState() {
+        return victoryState;
+    }
+
+    public Methods(int[] startingState, int[] victoryState) {
+        this.startingState = startingState;
+        this.victoryState = victoryState;
+
+    }
+
+    public boolean checkWinner(int[] boardInPlay) {
+        if (boardInPlay.equals(victoryState)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    // this method takes in a board and tells us how many parts are oop.
+    public int heuristic(int[] board) {
+        int i;
+        int heuristic=0;
+        for (i = 0; i < 8; i++) {
+            if (board[i] != i + 1) {
+                heuristic++;
+            }
+        }
+        if(board[8] != 0){
+            heuristic++;
+        }
+
+        return heuristic;
+    }
+    // g value function - pass in the glist and the f list
+    // spits out a g value
+    public int gValue(ArrayList<Integer> List, ArrayList<Integer> flist){
+        int g, f =0;
+        if (List.isEmpty()){
+            g=0;
+        }
+        else{
+            //get the minimum f index
+            int minIndex = flist.indexOf(Collections.min(flist));
+            g = List.get(minIndex);
+        }
+        return g;
+    }
+
+
+    public int indexOfZero(int[] board) {
+
+        int index=0;
+        int i;
+        for (i = 0; i < 9; i++) {
+            if (board[i] == 0) {
+                index = i;
+            }
+        }
+        return index;
+    }
+    //minimum heuristic value
+    public int indexNextState(ArrayList<Integer> list){
+        int index = 0;
+        if(list.isEmpty())
+        {
+            index = 0;
+        }
+        else {
+            index = list.indexOf(Collections.min(list));
+        }
+        return index;
+
+    }
+
+    public int[] moveLeft(int[] board) {
+        int index = indexOfZero(board);
+        board[index] = board[index-1];
+        board[index-1] = 0;
+        return board;
+    }
+
+
+    public int[] moveRight(int[] board) {
+        int index = indexOfZero(board);
+        board[index] = board[index+1];
+        board[index+1] = 0;
+        return board;
+    }
+
+
+    public int[] moveUp(int[] board) {
+        int index = indexOfZero(board);
+        board[index] = board[index-3];
+        board[index-3] = 0;
+        return board;
+    }
+
+    public int[] moveDown(int[] board) {
+        int index = indexOfZero(board);
+        board[index] = board[index+3];
+        board[index+3] = 0;
+        return board;
+    }
+
+
+  /*  public int compareTo(int[] firstBoard, int[] secondBoard) {
+        int i;
+        int heuristic1 = 0;
+        int heuristic2 = 0;
+        int comparison;
+        for (i = 0; i < 9; i++) {
+            if (firstBoard[i] != i + 1) {
+                heuristic1++;
+            }
+        }
+        for (i = 0; i < 9; i++) {
+            if (secondBoard[i] != i + 1) {
+                heuristic2++;
+            }
+        }
+        if(heuristic1>heuristic2){
+            comparison = 1;
+        }
+        else if(heuristic1<heuristic2){
+            comparison = -1;
+        }
+        else {
+            comparison = 0;
+        }
+        return comparison;
+        }
+*/
+}
